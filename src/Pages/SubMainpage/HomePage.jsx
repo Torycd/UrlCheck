8635d3df3b378184cb11dece3b72e2eeab9a692d
Torycd <vicidundun@gmail.com>
2024-08-07 22:15:56 +0100

    Homepage in progress

--- src/Pages/SubMainpage/HomePage.jsx
+++ src/Pages/SubMainpage/HomePage.jsx
@@ -2,8 +2,25 @@
 
 const HomePage = () => {
   return (
-    <div className="text-black">HomePage</div>
-  )
-}
+    <div className="text-black mx-20 text-xl">
+      <div className="grid grid-cols-2 pt-10">
+        <div>
+          <h2 className="text-3xl font-bold mb-5">VirusScope</h2>
+          <form type="submit" className="flex flex-col space-y-3">
+            <label>Submit a URL to VirusScope to check for malware</label>
+            <input
+              type="text"
+              className="bg-gray-300 px-4 py-3 rounded-md"
+              placeholder="https://example.com"
+            />
+          </form>
+        </div>
+        <div>
+          <h2 className="font-semibold text-3xl">No data yet</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
