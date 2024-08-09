67867d17988cdd0f858c0732ac104bcc95dc89d0
Torycd <vicidundun@gmail.com>
2024-08-09 22:57:40 +0100

    lets go

--- src/components/Analysis.jsx
+++ src/components/Analysis.jsx
@@ -24,48 +24,73 @@ const Analysis = ({ analysis }) => {
   ).toFixed(0);
 
   return (
-    <div>
-
-      <div style={{ width: "100%", height: 200 }}>
-        <ResponsiveContainer>
-          <PieChart>
-            <Pie
-              data={data}
-              cx="50%"
-              cy="50%"
-              innerRadius={60}
-              outerRadius={80}
-              fill="#8884d8"
-              paddingAngle={5}
-              dataKey="value"
-            >
-              {data.map((entry, index) => (
-                <Cell
-                  key={`cell-${index}`}
-                  fill={COLORS[index % COLORS.length]}
-                />
-              ))}
-              <Label
-                value={`${harmlessPercentage}%`}
-                position="center"
-                fill="#0000FF" // Color for the harmless label
-                fontSize={44}
-                fontWeight="bold"
-              />
-            </Pie>
-          </PieChart>
-        </ResponsiveContainer>
+    <div className="bg-gray-100 p-5 rounded-lg">
+      <div className="space-y-2">
+        <h2 className="text-2xl font-bold">{analysis.attributes.title}</h2>
+        <div className="grid grid-cols-2 gap-3">
+          <div style={{ width: "100%", height: 200 }}>
+            <ResponsiveContainer>
+              <PieChart>
+                <Pie
+                  data={data}
+                  cx="50%"
+                  cy="50%"
+                  innerRadius={60}
+                  outerRadius={80}
+                  fill="#8884d8"
+                  paddingAngle={5}
+                  dataKey="value"
+                >
+                  {data.map((entry, index) => (
+                    <Cell
+                      key={`cell-${index}`}
+                      fill={COLORS[index % COLORS.length]}
+                    />
+                  ))}
+                  <Label
+                    value={`${harmlessPercentage}%`}
+                    position="center"
+                    fill="#0000FF" // Color for the harmless label
+                    fontSize={44}
+                    fontWeight="bold"
+                  />
+                </Pie>
+              </PieChart>
+            </ResponsiveContainer>
+          </div>
+          <div>
+            <p>
+              <strong>Reputation:</strong> {analysis.attributes.reputation}
+            </p>
+            <p>
+              <strong>URL:</strong>{" "}
+              <a
+                href={analysis.attributes.url}
+                className="text-blue-600 underline"
+              >
+                {analysis.attributes.url}
+              </a>
+            </p>
+            <p>
+              <strong>Times Submitted:</strong>{" "}
+              {analysis.attributes["times_submitted"]}
+            </p>
+            <p>
+              <strong>Type:</strong> {analysis.attributes.type}
+            </p>
+            <p>
+              <strong>Harmless:</strong>{" "}
+              {analysis.attributes["total_votes"].harmless}
+            </p>
+            <p>
+              <strong>Malicious:</strong>{" "}
+              {analysis.attributes["total_votes"].malicious}
+            </p>
+          </div>
+        </div>
       </div>
 
       {/* Display additional analysis information */}
-
-      <p>Title: {analysis.attributes.title}</p>
-      <p>Reputation: {analysis.attributes.reputation}</p>
-      <p>URL: {analysis.attributes.url}</p>
-      <p>Times Submitted: {analysis.attributes["times_submitted"]}</p>
-      <p>Type: {analysis.attributes.type}</p>
-      <p>Harmless: {analysis.attributes["total_votes"].harmless}</p>
-      <p>Malicious: {analysis.attributes["total_votes"].malicious}</p>
     </div>
   );
 };
