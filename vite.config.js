a5fcd5c0239019320ce3057e085545c5d14eb92b
Torycd <vicidundun@gmail.com>
2024-08-12 23:54:21 +0100

    lets go

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'https://www.virustotal.com',
+      "/api": {
+        target: "https://www.virustotal.com", // Target VirusTotal API
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api/v3')
-      }
-    }
-  }
-})
+        rewrite: (path) => path.replace(/^\/api/, "/api/v3"), // Rewrite the path correctly
+      },
+    },
+  },
+});
