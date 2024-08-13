b3b6adafdc76f55cfcb8d8e59183635aac2ad9cb
Torycd <vicidundun@gmail.com>
2024-08-13 09:06:00 +0100

    CORS issues

--- src/Pages/SubMainpage/HomePage.jsx
+++ src/Pages/SubMainpage/HomePage.jsx
@@ -14,23 +14,20 @@ const HomePage = () => {
     const url = formData.get("url");
     const encodedUrl = btoa(url).replace(/=/g, "");
 
-    const apiBaseUrl = import.meta.env.MODE === 'development'
-      ? '/api' // Proxy path during development
-      : 'https://www.virustotal.com/api/v3';
+    const apiBaseUrl =
+      import.meta.env.MODE === "development"
+        ? "/api" // Proxy path during development
+        : "https://www.virustotal.com/api/v3";
     // to get the API Key from .env
     const apiKey = import.meta.env.VITE_API_KEY;
 
     try {
-      const response = await fetch(
-        `${apiBaseUrl}/urls/${encodedUrl}`,
-        {
-          headers: {
-            // "Content-Type": "application/json",
-            // mode: "no-cors",
-            "X-Apikey": apiKey,
-          },
-        }
-      );
+      const response = await fetch(`${apiBaseUrl}/urls/${encodedUrl}`, {
+        headers: {
+          accept: "application/json",
+          "X-Apikey": apiKey,
+        },
+      });
 
       if (!response.ok) {
         throw new Error(`Error: ${response.status} - ${response.statusText}`);
